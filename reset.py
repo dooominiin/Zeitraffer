5a471d8b479632c2fcfd307a0d8630980b87ea88
dooominiin <dominique.torti@gmx.ch>
2023-06-01 13:56:25 +0200

    save

--- reset.py
+++ reset.py
@@ -6,6 +6,7 @@ image_dir_path = "/home/raspi/Desktop/Zeitraffer/Images"
 video_dir_path = "/home/raspi/Desktop/Zeitraffer/Video"
 log_dir_path = "/home/raspi/Desktop/Zeitraffer/Log"
 
+
 # Delete directories if they exist
 if os.path.exists(image_dir_path):
     shutil.rmtree(image_dir_path)
