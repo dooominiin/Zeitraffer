2bd1f25d4d3d5a9e5e30edf77d8ad521972ad87f
dooominiin <dominique.torti@gmx.ch>
2023-06-05 21:21:14 +0200

    merge mit helligkeitsfilter implementiert

--- test.py
+++ test.py
@@ -29,14 +29,7 @@ def merge_Video(video_dir_path, merged_video_dir_path):
     
     # Check if merged video file already exists
     if os.path.exists(merged_video_file_path):
-        index = 1
-        while True:
-            # Append an index to the filename to avoid overwriting existing files
-            merged_video_filename = "timelapse_video{}_{}.".format(video_suffix, index) + video_format
-            merged_video_file_path = os.path.join(merged_video_dir_path, merged_video_filename)
-            if not os.path.exists(merged_video_file_path):
-                break
-            index += 1
+        os.remove(merged_video_file_path)
     
     # Create list of input video file paths
     video_file_paths = sorted([os.path.join(video_dir_path, filename) for filename in os.listdir(video_dir_path) if filename.startswith(video_filename_prefix) and filename.endswith(video_format)])
@@ -49,7 +42,7 @@ def merge_Video(video_dir_path, merged_video_dir_path):
 
     # Concatenate the videos using ffmpeg
     subprocess.call(["ffmpeg", "-f", "concat", "-safe", "0", "-i", "mylist.txt", "-vf", "setpts=0.125*PTS,select='not(mod(n,2))'", "-c:v", "libx264", "-preset", "ultrafast", merged_video_file_path])
-    return [merged_video_file_path] 
+    return merged_video_file_path 
 
 def check_brightness(frame):
     # Konvertieren Sie den Frame in den HSV-Farbraum
@@ -62,43 +55,47 @@ def check_brightness(frame):
     brightness_threshold = 20  # Beispielwert für die Helligkeitsschwelle
     
     # Überprüfen Sie die Helligkeit des Frames
-    is_not_bright = np.mean(v_channel) < brightness_threshold
+    is_not_bright = np.mean(v_channel) > brightness_threshold
     
-    return is_not_bright
+    return is_bright
 
-def create_delete_frames_array(input_video):
+def create_select_frames_array(input_video):
     # Video mit MoviePy öffnen
     video = VideoFileClip(input_video)
 
     # Array zum Speichern der Löschinformationen erstellen
-    delete_frames = []
+    select_frames = []
 
     # Schleife über jeden Frame im Video
     for idx, frame in enumerate(video.iter_frames(), start=1):
         # Überprüfen Sie die Helligkeit des Frames
         is_bright = check_brightness(Image.fromarray(frame))
         
-        # Hinzufügen des Löschstatus zum Array
-        delete_frames.append(not is_bright)
+        # Hinzufügen des select-status zum Array
+        select_frames.append(is_bright)
         
         print(f"Frame Nr. : {idx}")
 
     # Video-Objekt freigeben
     video.close()
 
-    return delete_frames
+    return select_frames
 
-def delete_frames_in_video(merged_video_file_path, delete_frames):
+def selected_frames_video(merged_video_file_path, select_frames):
     merged_video_dir_path = os.path.dirname(merged_video_file_path)
     merged_video_filename = os.path.basename(merged_video_file_path)
     filtered_video_file_path = os.path.join(merged_video_dir_path, "filtered_" + merged_video_filename)
-    my_select_string = select_string(delete_frames)
+    my_select_string = select_string(select_frames)
     
-    # ffmpeg-Befehl zum Löschen der Frames
+    # Check if merged video file already exists
+    if os.path.exists(filtered_video_file_path):
+        os.remove(filtered_video_file_path)
+
+    # ffmpeg-Befehl zum erstellen eines videos mit den hellen Frames
     ffmpeg_command = [
         "ffmpeg",
         "-threads",
-        "2",
+        "4",
         "-i",
         merged_video_file_path,
         "-vf",
@@ -124,21 +121,6 @@ def select_string(bool_array):
 
 
 if __name__ == "__main__":
-    #merged_video_file_path = merge_Video(video_dir_path, merged_video_dir_path)
-    print("hallo")
-    merged_video_file_path = "/home/raspi/Desktop/Zeitraffer/Video_komplett/timelapse_video_merged.mp4"
-    #merged_video_file_path = "/home/raspi/Desktop/Zeitraffer/Video/timelapse_video_0001.mp4"
-    delete_frames = create_delete_frames_array(merged_video_file_path)
-    
-    #delete_frames = [True] * 100
-
-    
-    # Array speichern
-    with open("delete_frames.pickle", "wb") as f:
-        pickle.dump(delete_frames, f)
-    # Array laden
-    with open("delete_frames.pickle", "rb") as f:
-        loaded_delet_frames = pickle.load(f)
-
-
-    delete_frames_in_video(merged_video_file_path, delete_frames)
+    merged_video_file_path = merge_Video(video_dir_path, merged_video_dir_path)
+    select_frames = create_select_frames_array(merged_video_file_path)
+    selected_frames_video(merged_video_file_path, select_frames)
