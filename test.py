5febfc459fe6f5f5b9345c532753abc7562fec72
dooominiin <dominique.torti@gmx.ch>
2023-06-05 15:34:40 +0200

    a

--- test.py
+++ test.py
@@ -1,6 +1,5 @@
 import moviepy
 
-print(moviepy.__version__)
 import os
 import subprocess
 import re
@@ -27,7 +26,18 @@ def merge_Video(video_dir_path, merged_video_dir_path):
     # Define output video file path
     merged_video_filename = "timelapse_video{}.".format(video_suffix) + video_format
     merged_video_file_path = os.path.join(merged_video_dir_path, merged_video_filename)
-
+    
+    # Check if merged video file already exists
+    if os.path.exists(merged_video_file_path):
+        index = 1
+        while True:
+            # Append an index to the filename to avoid overwriting existing files
+            merged_video_filename = "timelapse_video{}_{}.".format(video_suffix, index) + video_format
+            merged_video_file_path = os.path.join(merged_video_dir_path, merged_video_filename)
+            if not os.path.exists(merged_video_file_path):
+                break
+            index += 1
+    
     # Create list of input video file paths
     video_file_paths = sorted([os.path.join(video_dir_path, filename) for filename in os.listdir(video_dir_path) if filename.startswith(video_filename_prefix) and filename.endswith(video_format)])
 
@@ -52,9 +62,9 @@ def check_brightness(frame):
     brightness_threshold = 20  # Beispielwert für die Helligkeitsschwelle
     
     # Überprüfen Sie die Helligkeit des Frames
-    is_bright = np.mean(v_channel) > brightness_threshold
+    is_not_bright = np.mean(v_channel) < brightness_threshold
     
-    return is_bright
+    return is_not_bright
 
 def create_delete_frames_array(input_video):
     # Video mit MoviePy öffnen
@@ -66,7 +76,7 @@ def create_delete_frames_array(input_video):
     # Schleife über jeden Frame im Video
     for idx, frame in enumerate(video.iter_frames(), start=1):
         # Überprüfen Sie die Helligkeit des Frames
-        is_bright = check_brightness(frame)
+        is_bright = check_brightness(Image.fromarray(frame))
         
         # Hinzufügen des Löschstatus zum Array
         delete_frames.append(not is_bright)
@@ -78,22 +88,38 @@ def create_delete_frames_array(input_video):
 
     return delete_frames
 
-def delete_frames_in_video(input_video, output_video, delete_frames):
+def delete_frames_in_video(merged_video_file_path, delete_frames):
+    merged_video_dir_path = os.path.dirname(merged_video_file_path)
+    merged_video_filename = os.path.basename(merged_video_file_path)
+    filtered_video_file_path = os.path.join(merged_video_dir_path, "filtered_" + merged_video_filename)
+    
+    
     # ffmpeg-Befehl zum Löschen der Frames
     ffmpeg_command = [
         "ffmpeg",
         "-i",
-        input_video,
+        merged_video_file_path,
         "-vf",
-        f"select='not(eq(n\,{'+'.join(str(i) for i, delete in enumerate(delete_frames) if delete))})',setpts=N/FRAME_RATE/TB",
+        f"select='not(eq(n\,{''.join(str(i) for i, delete in enumerate(delete_frames) if delete)})',setpts=N/FRAME_RATE/TB",
         "-af",
-        f"aselect='not(eq(n\,{'+'.join(str(i) for i, delete in enumerate(delete_frames) if delete))})',asetpts=N/SR/TB",
+        f"aselect='not(eq(n\,{''.join(str(i) for i, delete in enumerate(delete_frames) if delete)})',asetpts=N/SR/TB",
         "-c:v",
-        "copy",
+        "libx264",
         "-c:a",
         "copy",
-        output_video
+        filtered_video_file_path
     ]
+
 
     # ffmpeg-Befehl ausführen
     subprocess.call(ffmpeg_command)
+
+
+
+if __name__ == "__main__":
+    #merged_video_file_path = merge_Video(video_dir_path, merged_video_dir_path)
+    print("hallo")
+    merged_video_file_path = "/home/raspi/Desktop/Zeitraffer/Video_komplett/timelapse_video_merged.mp4"
+    delete_frames = create_delete_frames_array(merged_video_file_path)
+    
+    delete_frames_in_video(merged_video_file_path, delete_frames)
