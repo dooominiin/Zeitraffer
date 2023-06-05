49a476d36b2da3e2e4d8aaeac415f1016a75f27d
dooominiin <dominique.torti@gmx.ch>
2023-06-05 22:01:36 +0200

    merge getestet, crontab 21:15 eingerichtet

--- merge_videos.py
+++ merge_videos.py
@@ -1,109 +1,126 @@
 import os
 import subprocess
-import re
-from moviepy.editor import VideoFileClip, concatenate
-from moviepy.editor import ImageSequenceClip
+from moviepy.editor import VideoFileClip
 import numpy as np
 from PIL import Image
 
-# Funktion zum Überprüfen der Helligkeit eines Frames
+def merge_Video(video_dir_path, merged_video_dir_path):
+    # Verzeichnis erstellen, wenn es noch nicht existiert
+    if not os.path.exists(merged_video_dir_path):
+        os.makedirs(merged_video_dir_path)
+
+    # Dateinamen-Präfix, Suffix und Videoformat definieren
+    video_filename_prefix = "timelapse_video"
+    video_suffix = "_merged"
+    video_format = "mp4"
+
+    # Pfad zur Ausgabedatei des zusammengeführten Videos festlegen
+    merged_video_filename = "timelapse_video{}.".format(video_suffix) + video_format
+    merged_video_file_path = os.path.join(merged_video_dir_path, merged_video_filename)
+    
+    # Überprüfen, ob die zusammengeführte Videodatei bereits existiert
+    if os.path.exists(merged_video_file_path):
+        os.remove(merged_video_file_path)
+    
+    # Liste der Eingabevideodateipfade erstellen
+    video_file_paths = sorted([os.path.join(video_dir_path, filename) for filename in os.listdir(video_dir_path) if filename.startswith(video_filename_prefix) and filename.endswith(video_format)])
+
+    # video_file_paths in mylist.txt schreiben
+    with open("mylist.txt", "w") as f:
+        for path in video_file_paths:
+            f.write("file '{}'\n".format(path))
+            print("file '{}'\n".format(path))
+
+    # Videos mit ffmpeg zusammenführen
+    subprocess.call(["ffmpeg", "-f", "concat", "-safe", "0", "-i", "mylist.txt", "-vf", "setpts=0.125*PTS,select='not(mod(n,2))'", "-c:v", "libx264", "-preset", "ultrafast", merged_video_file_path])
+    os.remove("mylist.txt")
+    return merged_video_file_path 
+
 def check_brightness(frame):
-    # Konvertieren Sie den Frame in den HSV-Farbraum
+    # Frame in den HSV-Farbraum konvertieren
     hsv_frame = frame.convert("HSV")
     
-    # Extrahieren Sie den V-Kanal (Helligkeitskanal)
+    # V-Kanal (Helligkeitskanal) extrahieren
     v_channel = np.array(hsv_frame)[:, :, 2]
     
-    # Definieren Sie Ihre Bedingungen für die Helligkeit
+    # Helligkeitsschwelle definieren
     brightness_threshold = 20  # Beispielwert für die Helligkeitsschwelle
     
-    # Überprüfen Sie die Helligkeit des Frames
+    # Helligkeit des Frames überprüfen
     is_bright = np.mean(v_channel) > brightness_threshold
     
     return is_bright
 
-
-# Define directory paths
-video_dir_path = "/home/raspi/Desktop/Zeitraffer/Video"
-merged_video_dir_path = "/home/raspi/Desktop/Zeitraffer/Video_komplett"
-
-# Create directory if it doesn't exist
-if not os.path.exists(merged_video_dir_path):
-    os.makedirs(merged_video_dir_path)
-
-# Define file name prefix, suffix, and video format
-video_filename_prefix = "timelapse_video"
-video_suffix = "_merged"
-video_format = "mp4"
-
-# Define output video file path
-merged_video_filename = "timelapse_video{}.".format(video_suffix) + video_format
-merged_video_file_path = os.path.join(merged_video_dir_path, merged_video_filename)
-
-# Check if merged video file already exists
-if os.path.exists(merged_video_file_path):
-    index = 1
-    while True:
-        # Append an index to the filename to avoid overwriting existing files
-        merged_video_filename = "timelapse_video{}_{}.".format(video_suffix, index) + video_format
-        merged_video_file_path = os.path.join(merged_video_dir_path, merged_video_filename)
-        if not os.path.exists(merged_video_file_path):
-            break
-        index += 1
-
-# Create list of input video file paths
-video_file_paths = sorted([os.path.join(video_dir_path, filename) for filename in os.listdir(video_dir_path) if filename.startswith(video_filename_prefix) and filename.endswith(video_format)])
-
-# Write video_file_paths to mylist.txt
-with open("mylist.txt", "w") as f:
-    for path in video_file_paths:
-        f.write("file '{}'\n".format(path))
-        print("file '{}'\n".format(path))
-
-# Concatenate the videos using ffmpeg
-subprocess.call(["ffmpeg", "-f", "concat", "-safe", "0", "-i", "mylist.txt", "-vf", "setpts=0.125*PTS,select='not(mod(n,2))'", "-c:v", "libx264", "-preset", "ultrafast", merged_video_file_path])
-
-
-
-
-
-
-
-
-# Filter the frames based on brightness using ffmpeg
-merged_video_dir_path = os.path.dirname(merged_video_file_path)
-merged_video_filename = os.path.basename(merged_video_file_path)
-filtered_video_file_path = os.path.join(merged_video_dir_path, "filtered_" + merged_video_filename)
-
-while True:
+def create_select_frames_array(input_video):
     # Video mit MoviePy öffnen
-    video = VideoFileClip(merged_video_file_path)
+    video = VideoFileClip(input_video)
 
-    # Neuen Video-Clip erstellen
-    filtered_video = None
+    # Array zum Speichern der Informationen über ausgewählte Frames erstellen
+    select_frames = []
 
     # Schleife über jeden Frame im Video
     for idx, frame in enumerate(video.iter_frames(), start=1):
-        # Konvertieren Sie den Frame in ein Image-Objekt
-        frame_image = Image.fromarray(frame)
-
-        # Überprüfen Sie die Helligkeit des Frames
-        is_bright = check_brightness(frame_image)
-
-        # Frame zum gefilterten Video hinzufügen, wenn Helligkeitsbedingung erfüllt ist
-        if is_bright:
-            if filtered_video is None:
-                # Erstellen Sie das gefilterte Video und speichern Sie den aktuellen Frame als Startpunkt
-                filtered_video = ImageSequenceClip([frame], fps=video.fps)
-            else:
-                # Fügen Sie den aktuellen Frame zum gefilterten Video hinzu
-                filtered_video = concatenate([filtered_video, ImageSequenceClip([frame], fps=video.fps)])
-
+        # Helligkeit des Frames überprüfen
+        is_bright = check_brightness(Image.fromarray(frame))
+        
+        # Auswählen des Frames und Status zum Array hinzufügen
+        select_frames.append(is_bright)
+        
         print(f"Frame Nr. : {idx}")
 
-    # Speichern Sie das gefilterte Video
-    filtered_video.write_videofile(filtered_video_file_path)
-
-    # Video-Objekte freigeben
+    # Video-Objekt freigeben
     video.close()
-    filtered_video.close()
+
+    return select_frames
+
+def create_selected_frames_video(merged_video_file_path, select_frames):
+    merged_video_dir_path = os.path.dirname(merged_video_file_path)
+    merged_video_filename = os.path.basename(merged_video_file_path)
+    filtered_video_file_path = os.path.join(merged_video_dir_path, "filtered_" + merged_video_filename)
+    my_select_string = select_string(select_frames)
+    
+    # Überprüfen, ob die gefilterte Videodatei bereits existiert
+    if os.path.exists(filtered_video_file_path):
+        os.remove(filtered_video_file_path)
+
+    # ffmpeg-Befehl zum Erstellen eines Videos mit den hellen Frames
+    ffmpeg_command = [
+        "ffmpeg",
+        "-threads",
+        "4",
+        "-i",
+        merged_video_file_path,
+        "-vf",
+        my_select_string,
+        "-preset",
+        "ultrafast",
+        "-c:v",
+        "libx264",
+        "-c:a",
+        "copy",
+        filtered_video_file_path
+    ]
+
+    print(ffmpeg_command)
+
+    # ffmpeg-Befehl ausführen
+    subprocess.call(ffmpeg_command)
+
+def select_string(bool_array):
+    # Liste der Indexe erstellen, bei denen der Wert True ist
+    selected_frames = [str(i) for i, is_frame_bright in enumerate(bool_array) if is_frame_bright]
+    
+    # Den String im gewünschten Format erstellen
+    ffmpeg_select_string = "+".join([f"eq(n,{frame_index})" for frame_index in selected_frames])
+    
+    return f"select='{ffmpeg_select_string}',setpts=N/FRAME_RATE/TB"
+
+
+if __name__ == "__main__":
+    # Verzeichnispfade definieren
+    video_dir_path = "/home/raspi/Desktop/Zeitraffer/Video"
+    merged_video_dir_path = "/home/raspi/Desktop/Zeitraffer/Video_komplett"
+    
+    merged_video_file_path = merge_Video(video_dir_path, merged_video_dir_path)
+    select_frames = create_select_frames_array(merged_video_file_path)
+    create_selected_frames_video(merged_video_file_path, select_frames)
